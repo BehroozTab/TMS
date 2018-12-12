70dcdb1c4af8f58c6159aba7589138143c720c9e
Behrooz Tabrizian <t_behrooz2000@yahoo.com>
2018-12-12 09:42:10 -0500

    Add files via upload

---
+++ TMS_Project.java
@@ -0,0 +1,16 @@
+
+package tms_project;
+
+import java.io.IOException;
+
+
+public class TMS_Project {
+
+    
+    public static void main(String[] args) throws IOException {
+      
+                LoginGUI GUI = new LoginGUI();
+
+    }
+    
+}
